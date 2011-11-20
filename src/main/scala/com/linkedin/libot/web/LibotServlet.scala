74760491eb496cf7a1f01b7452c4e55bf7f42628
Pablo Fernandez <fernandezpablo85@gmail.com>
2011-11-20 17:12:00 -0300

    moved servlet outside Web object

---
+++ src/main/scala/com/linkedin/libot/web/LibotServlet.scala
@@ -0,0 +1,28 @@
+package com.linkedin.libot.web
+
+import com.twitter.finagle.Service
+import org.jboss.netty.handler.codec.http.{HttpResponse, HttpRequest}
+import com.twitter.util.Future
+import com.linkedin.libot.util.ParamsExtractor
+import com.linkedin.libot.model.Dispatcher
+import com.twitter.finagle.http.Response._
+import com.twitter.util.Future._
+import com.twitter.finagle.http.Response
+
+/**
+ * @author: Pablo Fernandez
+ */
+
+class LibotServlet extends Service[HttpRequest, HttpResponse]
+{
+  def apply (req: HttpRequest): Future[HttpResponse] =
+  {
+    val rawInput = new String(req.getContent.array, "UTF-8")
+    val params = ParamsExtractor.extract(rawInput, "userkey", "msg")
+    val responseMessage = Dispatcher.handle(params)
+    val response = Response()
+    response.setStatusCode(200)
+    response.setContentString(responseMessage.toString)
+    Future(response)
+  }
+}
